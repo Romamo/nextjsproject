f3eec65fc79c541e4288911ff21f9415376aa19f
Vyacheslav <drimstor@gmail.com>
2022-09-01 16:29:59 +0300

    function check

--- public/script.js
+++ public/script.js
@@ -3,11 +3,11 @@
   const btnOff = document.querySelector("#btnOff");
   btnOn.addEventListener("click", function () {
     localStorage.setItem("JS", "ON");
-    setTimeout(( ) => window.location.reload(), 500)
+    // setTimeout(( ) => window.location.reload(), 500)
   });
   btnOff.addEventListener("click", function () {
     localStorage.clear();
     // window.location.reload();
-    setTimeout(( ) => window.location.reload(), 500)
+    setTimeout(() => window.location.reload(), 300);
   });
 })();
