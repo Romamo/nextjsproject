9b9a4f07c24c8e9f36e0599d5d1d5a1c7244cbdb
Vyacheslav <drimstor@gmail.com>
2022-09-05 12:28:28 +0300

    mui changed

--- pages/_document.js
+++ pages/_document.js
@@ -26,7 +26,7 @@ export default class MyDocument extends Document {
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
           />
-          <style dangerouslySetInnerHTML={createFontStyle()} />
+          {/* <style dangerouslySetInnerHTML={createFontStyle()} /> */}
         </Head>
         <body>
           <Main />
@@ -41,26 +41,6 @@ export default class MyDocument extends Document {
 // it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
   // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
 
   const originalRenderPage = ctx.renderPage;
 
