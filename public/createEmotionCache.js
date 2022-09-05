8708accd9817be99b2269a8a32805227126baf31
Vyacheslav <drimstor@gmail.com>
2022-09-05 12:01:31 +0300

    mui changed

--- public/createEmotionCache.js
+++
@@ -1,9 +0,0 @@
-import createCache from '@emotion/cache';
-
-export default function createEmotionCache() {
-  return createCache({ key: 'css' });
-}
-
-  // return createCache({ key: 'mui-style', prepand: false });
-
-
