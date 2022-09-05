4e7181ce87e683512bd20059d29c7ec541c3d468
Vyacheslav <drimstor@gmail.com>
2022-09-05 17:36:28 +0300

    404 page added

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/link";
+import s from "../styles/Home.module.scss";
+import Button from "@mui/material/Button";
+
+function FourOhFour() {
+  return (
+    <div className={s.FourOhFour}>
+      <div className={s.wrapper}>
+        <div className={s.blur}></div>
+        <h1>404</h1>
+        <span>Opps! Page not found</span>
+        <p>Sorry, the page you're looking for doesn't exist.</p>
+        <Link href="/">
+          <Button variant="contained">BACK TO HOME</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default FourOhFour;
--- public/script.js
+++ public/script.js
@@ -1,10 +1,5 @@
-(function () {
-  const btnOn = document.querySelector("#btnOn");
-  const btnOff = document.querySelector("#btnOff");
-  btnOn.addEventListener("click", function () {
+if (document.querySelector("#btnOn")) {
+  document.querySelector("#btnOn").addEventListener("click", function () {
     document.cookie = "name=user;max-age=30";
   });
-  btnOff.addEventListener("click", function () {
-    document.cookie = "name=user;max-age=-1";
-  });
-})();
+}
